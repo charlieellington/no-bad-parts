90ba66ae72ba31afb95e69cb0afb96293ece5dea
balancedcharlie <charliee@gmail.com>
2025-06-05 11:06:43 +0200

    feat(chatway): embed Chatway widget globally via app/head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      {/* Chatway chat widget */}
+      <script
+        id="chatway"
+        async
+        src="https://cdn.chatway.app/widget.js?id=6qxBzuIMeu2h"
+      />
+    </>
+  );
+} 
